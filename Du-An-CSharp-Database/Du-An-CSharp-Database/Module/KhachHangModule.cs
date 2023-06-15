e4b8e80ce85e42bc7c0bbcfbdd34eff400d6f911
K4tsuJr <120944583+Mrk4tsu@users.noreply.github.com>
2023-06-15 07:46:38 +0700

    Định nghĩa database trong README

---
+++ Du-An-CSharp-Database/Du-An-CSharp-Database/Module/KhachHangModule.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Du_An_CSharp_Database.Module
+{
+    public class KhachHangModule
+    {
+        [Required]
+        [MaxLength(50)]
+        public string TenKH { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string HoKH { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string Usename { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string Password { get; set; }
+        public string Mail { get; set; }
+        [MaxLength(12)]
+        public string SDT { get; set; }
+        [MaxLength(13)]
+        public string CCCD { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string DiaChi { get; set; }
+    }
+}
